267ae67a05e873779709f8022a90e6ea5f43d9e9
sdu <sadubois@live.be>
2024-05-29 23:33:49 +0200

    add debugging

--- main.py
+++ main.py
@@ -9,11 +9,8 @@ from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from PyQt6 import uic
 
-import numpy as np
-
 import lcm
 import widgets as wid
-import resources as res
 from lcm import *
 from PIL import Image, ImageOps
 
@@ -21,6 +18,14 @@ import torch
 import os
 import gc
 import csv
+import logging
+import traceback
+import sys
+
+# basic logger functionality
+log = logging.getLogger(__name__)
+handler = logging.StreamHandler(stream=sys.stdout)
+log.addHandler(handler)
 
 # Params
 BASE_DIR = res.find('img/AI_ref_images_bo')
@@ -1255,6 +1260,47 @@ def main(argv=None):
     # no errors since we're not running our own event loop
     return 0
 
+def show_exception_box(log_msg):
+    """Checks if a QApplication instance is available and shows a messagebox with the exception message.
+    If unavailable (non-console application), log an additional notice.
+    """
+    if QApplication.instance() is not None:
+        errorbox = QMessageBox()
+        errorbox.setText("Oops. An unexpected error occured:\n{0}".format(log_msg))
+        errorbox.exec()
+    else:
+        log.debug("No QApplication instance available.")
+
+class UncaughtHook(QObject):
+    _exception_caught = pyqtSignal(object)
+
+    def __init__(self, *args, **kwargs):
+        super(UncaughtHook, self).__init__(*args, **kwargs)
+
+        # this registers the exception_hook() function as hook with the Python interpreter
+        sys.excepthook = self.exception_hook
+
+        # connect signal to execute the message box function always on main thread
+        self._exception_caught.connect(show_exception_box)
+
+    def exception_hook(self, exc_type, exc_value, exc_traceback):
+        """Function handling uncaught exceptions.
+        It is triggered each time an uncaught exception occurs.
+        """
+        if issubclass(exc_type, KeyboardInterrupt):
+            # ignore keyboard interrupt to support console applications
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        else:
+            exc_info = (exc_type, exc_value, exc_traceback)
+            log_msg = '\n'.join([''.join(traceback.format_tb(exc_traceback)),
+                                 f'{exc_type.__name__}: {exc_value}'])
+            log.critical(f"Uncaught exception:\n {log_msg}", exc_info=exc_info)
+
+            # trigger message box show
+            self._exception_caught.emit(log_msg)
+
+# create a global instance of our class to register the hook
+qt_exception_hook = UncaughtHook()
 
 if __name__ == '__main__':
     import sys
